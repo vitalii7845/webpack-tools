51f0f3a73ac93dc4085bc2d8e0afd766fc2b5c93
Vitalii.Khomyk <vitalii.khomuk@gmail.com>
2022-08-05 01:28:23 +0300

    task2

---
+++ lesson1/task2/src/scripts/createTask.js
@@ -0,0 +1,26 @@
+import { renderTasks } from './renderer.js';
+import { getTasksList, creatTask } from './tasksGateway.js';
+
+function onCreateTask() {
+  const taskTitleInputElem = document.querySelector('.task-input');
+  const text = taskTitleInputElem.value;
+
+  if (!text) return;
+
+  const newTask = {
+    id: Math.random() * 100 + 1,
+    text,
+    done: false,
+    createDate: new Date().toISOString(),
+  };
+
+  creatTask(newTask)
+    .then(() => getTasksList())
+    .then(newTasksList => {
+      renderTasks(newTasksList);
+    });
+  console.log(newTask);
+  taskTitleInputElem.value = '';
+}
+
+export { onCreateTask };
