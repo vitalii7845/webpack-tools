bdd2a5e288584df841b60441d397e5a66b2eddef
Vitalii.Khomyk <vitalii.khomuk@gmail.com>
2022-08-09 00:55:24 +0300

    commit l6-t1

---
+++ lesson6/task1/webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /.(jpg|png)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: '[name].[ext]',
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
